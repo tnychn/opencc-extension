7114bd8b0f653e7cd98bb9ff1a0dbe6827bdaf4b
tnychn <tonychan2342@gmail.com>
2021-07-09 17:41:26 +0800

    fix: context menu item creation

--- src/background.js
+++ src/background.js
@@ -1,12 +1,10 @@
 /* Create context menu item for converting selected text only */
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    title: "Convert",
-    contexts: ["selection"],
-    onclick: (info) => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "select", text: info.selectionText });
-      });
-    },
-  });
+chrome.contextMenus.create({
+  title: "Convert Chinese Characters",
+  contexts: ["selection"],
+  onclick: (info) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "select", text: info.selectionText });
+    });
+  },
 });
